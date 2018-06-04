48bfc03cf92808344371d1ca7ace0041d8262071
yakovina <sasha.yakovina@gmail.com>
2018-06-04 15:32:06 +0300

    add whole css without media

--- js/main.js
+++ js/main.js
@@ -2,12 +2,25 @@ $(document).ready(function(){
   //  $(".navigation__item").scrollTop(20);
     $(".owl-carousel").owlCarousel({
         items: 1,
-        autoplay:false, //Автозапуск слайдера
-        smartSpeed:1000, //Время движения слайда
-        autoplayTimeout:2000,
         loop: true,
-        nav:false,
+        nav: true,
+        margin:10,
         animateInClass: true,
-        dots: false
+        dots: false,
+        navText: ["<img src='img/arrowleft.png'>", "<img src='img/arrowright.png'>"],
     });
   });
+
+
+  var pictures =   Array.prototype.slice.call(document.getElementsByClassName("article__img"));
+  var picturesWrapp =  document.querySelector(".pictureswrapper");
+
+pictures.sort(function(a,b){
+  return a.dataset.number - b.dataset.number;
+});
+
+pictures.forEach(function(item, i, pictures) {
+  var item2 = item.cloneNode(true);
+  item2.classList.add("article__img-small");
+  picturesWrapp.appendChild(item2);
+});
