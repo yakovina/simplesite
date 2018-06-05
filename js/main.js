b281fdbe24be493d9bd583f1951b751c7a972303
yakovina <sasha.yakovina@gmail.com>
2018-06-05 23:30:42 +0300

    added burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+
+var pictures =   Array.prototype.slice.call(document.getElementsByClassName("article__img"));
+var picturesWrapp =  document.querySelector(".pictureswrapper");
+var modal = document.querySelector('.modal');
+var burger = document.querySelector('.header__burger');
+var close = document.querySelector('.close');
+var nav = document.getElementsByClassName('js-nav__list');
+var MenuItems=[];
+var MenuItemsModal=[];
+
+addPic(pictures, picturesWrapp);
+
+MenuItems=Array.prototype.slice.call(document.getElementsByClassName('js-nav__list'));
+MenuItemsModal = cloneNodeClass(MenuItems);
+burger.addEventListener('click', initBurger); 
+close.addEventListener('click', closeBurger); 
+for (i = 0; i < MenuItemsModal.length; i++) {
+  MenuItemsModal[i].addEventListener('click', closeBurger);
+}
+
+
+function initBurger(){
+
+  for (var i = 0; i < MenuItemsModal.length; i++) {
+      modal.appendChild(MenuItemsModal[i]); 
+    }
+
+  modal.className += " opened";
+  for (var i = 0; i < MenuItemsModal.length; i++) {
+    MenuItemsModal[i].className +=" nav-open";
+    }
+}
+
+
+//ADD SLIDER
+
 $(document).ready(function(){
   //  $(".navigation__item").scrollTop(20);
     $(".owl-carousel").owlCarousel({
@@ -11,16 +47,27 @@ $(document).ready(function(){
     });
   });
 
+  function addPic(pictures, wrrapper){
+    pictures.sort(function(a,b){
+      return a.dataset.number - b.dataset.number;
+    });
+  
+    pictures.forEach(function(item, i, pictures) {
+      var item2 = item.cloneNode(true);
+      item2.classList.add("article__img-small");
+      wrrapper.appendChild(item2);
+    });
 
-  var pictures =   Array.prototype.slice.call(document.getElementsByClassName("article__img"));
-  var picturesWrapp =  document.querySelector(".pictureswrapper");
+}
 
-pictures.sort(function(a,b){
-  return a.dataset.number - b.dataset.number;
-});
+function cloneNodeClass(arr){
+    var arr2=[];
+    for (var i = 0; i < arr.length; i++) {
+        arr2[i]=arr[i].cloneNode(true);
+      }
+    return arr2;
+}
 
-pictures.forEach(function(item, i, pictures) {
-  var item2 = item.cloneNode(true);
-  item2.classList.add("article__img-small");
-  picturesWrapp.appendChild(item2);
-});
+function closeBurger(){
+  modal.classList.remove('opened');
+}
